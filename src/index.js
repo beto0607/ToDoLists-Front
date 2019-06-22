773fc93ee314d1e87f43964fb660429bdff5b1cc
Roberto Albanesi <beto0607@gmail.comn>
2019-06-22 12:16:31 +0200

    Created React app. Added Routing, Yup, Formik. Added Authentication route. Added Login and Register components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './scss/_main.scss';
 import App from './App';
+import Authentication from './containers/Authentication';
+import NotFound from './containers/NotFound';
 import * as serviceWorker from './serviceWorker';
+import { Switch, Route,  BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+	  <Switch>
+      	<Route exact path="/" component={App} />
+  	    <Route path="/auth" component={Authentication} />
+		<Route component={NotFound}/>
+      </Switch>
+  	</div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
